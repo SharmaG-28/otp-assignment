37ed576c4a0225faf85043c53f2b6d06c2494e77
Gautam Sharma <sharmagautam2811@gmail.com>
2024-02-01 00:19:17 +0530

    OTP version 3

--- OtpGenerator.py
+++ OtpGenerator.py
@@ -1,68 +1,77 @@
 #Importing Libraries
-from twilio.rest import Client
 import random
 import smtplib
+from twilio.rest import Client
 
-# Constants
-SENDERS_EMAIL = 'sharmagautam2811@gmail.com'
-SENDERS_PASSWORD = 'purjnoylbryghwaz'
-TWILIO_ACCOUNT_SID = 'ACbe42830956f02da22e321dff91ab99f1'
-TWILIO_AUTH_TOKEN = '3f879200374cc6bfcca3e0617f86593e'
-TWILIO_NUMBER = '+17272314733'
+# Class OTPGenerator starts here
+class OTPGenerator:
+    def __init__(self, sender_email, sender_password, twilio_account_sid, twilio_auth_token, twilio_number):
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+        self.twilio_account_sid = twilio_account_sid
+        self.twilio_auth_token = twilio_auth_token
+        self.twilio_number = twilio_number
 
-#Generating otp
-def generate_otp():
-    return random.randint(100000, 999999)
+    # Method for Generating otp
+    def generate_otp(self):
+        return random.randint(100000, 999999)
 
-#Validating email
-def validate_email(email):
-    return '@' in email and '.' in email
+    # Method for Validating email
+    def validate_email(self, email):
+        return '@' in email and '.' in email
 
-#Validating mobile number
-def validate_mobile(mobile):
-    return len(mobile) == 10 and mobile.isdigit()
+    # Method for Validating mobile number
+    def validate_mobile(self, mobile):
+        return len(mobile) == 10 and mobile.isdigit()
 
-#Sending otp over email
-def send_otp_over_email(receiver_email, otp):
-    message = 'Your OTP is ' + str(otp)
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(SENDERS_EMAIL, SENDERS_PASSWORD)
-    server.sendmail(SENDERS_EMAIL, receiver_email, message)
-    server.quit()
-    print("OTP sent successfully to", receiver_email)
+    # Method for Sending otp over email
+    def send_otp_over_email(self, receiver_email, otp):
+        if self.validate_email(receiver_email):
+            message = 'Your OTP is ' + str(otp)
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(self.sender_email, self.sender_password)
+            server.sendmail(self.sender_email, receiver_email, message)
+            server.quit()
+            print("OTP sent successfully to", receiver_email)
+        else:
+            print("Please enter a valid email!")
 
-#Sending otp over sms
-def send_otp_over_mobile(target, otp):
-    if validate_mobile(target):
-        target = "+91" + str(target)
-        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-        message = client.messages.create(
-            body="Your OTP is " + str(otp),
-            from_=TWILIO_NUMBER,
-            to=target
-        )
-        print("OTP Sent to {} successfully".format(target))
-    else:
-        print("Enter a valid mobile number!")
+    # Method for Sending otp over sms
+    def send_otp_over_mobile(self, target, otp):
+        if self.validate_mobile(target):
+            target = "+91" + str(target)
+            client = Client(self.twilio_account_sid, self.twilio_auth_token)
+            message = client.messages.create(
+                body="Your OTP is " + str(otp),
+                from_=self.twilio_number,
+                to=target
+            )
+            print("OTP Sent to {} successfully".format(target))
+        else:
+            print("Enter a valid mobile number!")
 
-#Main
-def main():
-    print("\n*******************<< OTP Generator >>*******************")
-    otp = generate_otp()
+    #Main method
+    def main(self):
+        print("\n*******************<< OTP Generator >>*******************")
+        otp = self.generate_otp()
 
-    # Sending OTP over Email
-    print("\nSending OTP via Email")
-    receiver_email = input("Enter your Email: ")
-    if validate_email(receiver_email):
-        send_otp_over_email(receiver_email, otp)
-    else:
-        print("Please enter a valid email!")
+        # Sending OTP over Email
+        print("\nSending OTP via Email")
+        receiver_email = input("Enter your Email: ")
+        self.send_otp_over_email(receiver_email, otp)
 
-    # Sending OTP over SMS
-    print("\nSending OTP via SMS")
-    target_mobile = input("Enter Mobile number: ")
-    send_otp_over_mobile(target_mobile, otp)
+        # Sending OTP over SMS
+        print("\nSending OTP via SMS")
+        target_mobile = input("Enter Mobile number: ")
+        self.send_otp_over_mobile(target_mobile, otp)
 
 if __name__ == "__main__":
-    main()
+    sender_email = 'sharmagautam2811@gmail.com'
+    sender_password = 'purjnoylbryghwaz'
+    twilio_account_sid = 'ACbe42830956f02da22e321dff91ab99f1'
+    twilio_auth_token = '3f879200374cc6bfcca3e0617f86593e'
+    twilio_number = '+17272314733'
+
+    otp_generator = OTPGenerator(sender_email, sender_password, twilio_account_sid, twilio_auth_token, twilio_number)
+    otp_generator.main()
