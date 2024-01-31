6e8e8e636c8970bc36ca82c99b81f40427ab00ec
Gautam Sharma <sharmagautam2811@gmail.com>
2024-02-01 00:20:45 +0530

    Test Cases v3

--- test_otp_generator.py
+++ test_otp_generator.py
@@ -1,24 +1,36 @@
-import unittest as UT
 import pytest
-import OtpGenerator_Version_3 as OTP
-class TestOtp(UT.TestCase):
-    def test_validatemail(self):
-        result = OTP.validatemail("sharmagautam2811@gmai.com")
-        expected = True
-        self.assertEqual(result, expected)
+from OtpGenerator import OTPGenerator
 
-    def test_validatemail(self):
-        result = OTP.validatemail("sharmagautamgmaicom")
-        expected = True
-        self.assertEqual(result, expected)
+@pytest.fixture
+def otp_generator():
+    sender_email = 'sharmagautam2811@gmail.com'
+    sender_password = 'purjnoylbryghwaz'
+    twilio_account_sid = 'ACbe42830956f02da22e321dff91ab99f1'
+    twilio_auth_token = '3f879200374cc6bfcca3e0617f86593e'
+    twilio_number = '+17272314733'
 
-    def test_generateotp(self):
-        otp_len = len(OTP.generateotp())
-        expected_len = 6
-        self.assertEqual(otp_len, expected_len)
-        resut_type = OTP.generateotp()
-        otp_type = resut_type.isdigit()
-        expected_type = True
-        self.assertEqual(otp_type, expected_type)
-if __name__ == '__main__':
-    pytest.main()
+    return OTPGenerator(sender_email, sender_password, twilio_account_sid, twilio_auth_token, twilio_number)
+
+def test_validate_email_valid():
+    otp_generator_instance = otp_generator()
+    result = otp_generator_instance.validate_email("sharmagautam2811@gmail.com")
+    expected = True
+    assert result == expected
+
+def test_validate_email_invalid():
+    otp_generator_instance = otp_generator()
+    result = otp_generator_instance.validate_email("sharmagautamgmaicom")
+    expected = False
+    assert result == expected
+
+def test_generate_otp():
+    otp_generator_instance = otp_generator()
+    generated_otp = otp_generator_instance.generate_otp()
+    otp_str = str(generated_otp)
+    otp_len = len(otp_str)
+    expected_len = 6
+    assert otp_len == expected_len
+
+    otp_type = otp_str.isdigit()
+    expected_type = True
+    assert otp_type == expected_type
